fa82a16292b0931ac23edf3450be715b9407dac4
ravi591 <ravindra.pof@gmail.com>
2026-03-19 22:31:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+body {
+  font-family: Arial;
+  text-align: center;
+  background: #f5fff5;
+}
+
+.products {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+  padding: 20px;
+}
+
+.product {
+  border: 1px solid #ddd;
+  padding: 15px;
+  background: white;
+  border-radius: 10px;
+}
+
+button {
+  background: green;
+  color: white;
+  padding: 10px;
+  border: none;
+  cursor: pointer;
+}
